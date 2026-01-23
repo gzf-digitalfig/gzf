126227b02d22e8c21c919f481796b96df763c288
ctrl-freelance <admin@ctrl-freelance.com>
2026-01-23 09:35:23 +0000

    revamp

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,11 +9,14 @@ export default function Navbar() {
   useEffect(() => {
     if (isMenuOpen) {
       document.body.style.overflow = 'hidden';
+      document.documentElement.style.overflow = 'hidden';
     } else {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
     }
     return () => {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = '';
+      document.documentElement.style.overflow = '';
     };
   }, [isMenuOpen]);
 
